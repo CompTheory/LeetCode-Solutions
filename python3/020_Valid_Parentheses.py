4fdce7f8437faf7cf12f02548f3502f1972b8e58
CompTheory <arshia.1384momtazi@gmail.com>
2025-12-14 03:48:12 +0330

    Refactor Valid Parentheses solution with comments
    Added comments for time and space complexity, and example usage.

--- python3/020_Valid_Parentheses.py
+++ python3/020_Valid_Parentheses.py
@@ -1,3 +1,9 @@
+"""
+LeetCode #20 - Valid Parentheses
+Youtube Video: https://www.youtube.com/watch?v=_MSzYuzGydk&t=4s
+"""
+
+
 class Solution:
     def isValid(self, s: str) -> bool:
         hashmap = {")" : "(", "}" : "{", "]" : "["}
@@ -12,4 +18,14 @@ class Solution:
                 last = stack.pop()
                 if last != hashmap[var]:
                     return False
+
         return not stack
+
+
+# time complexity = O(n)
+# space complexity = O(n)
+
+
+# # Example Usage:
+# Input: s = "()[]{}"
+# Output: true
