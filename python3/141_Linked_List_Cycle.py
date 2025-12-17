374c038e9b947674a99b3118d099574d9f52fefe
CompTheory <arshia.1384momtazi@gmail.com>
2025-12-17 23:43:37 +0330

    Add files via upload
    A clean and well-commented Python solution to LeetCode Problem #141: Linked List Cycle. This solution uses the fast and slow pointer (Floyd’s Cycle Detection) technique to efficiently detect cycles in a linked list, with clear explanations and time/space complexity analysis. Part of my coding interview solutions collection.

---
+++ python3/141_Linked_List_Cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slower = head
+        faster = head
+        while faster:
+            slower = slower.next
+            faster = faster.next.next if faster.next else faster.next
+            if slower == faster and slower:
+                return True
+        return False
+
+
+# time complexity = O(n)
+# space complexity = O(1)
